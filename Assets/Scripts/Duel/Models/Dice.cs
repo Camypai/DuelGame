3e06f7a4c0d72f2fdf8b82b749dfe5237ac42b08
Илья <igorshkov.93@yandex.ru>
2020-05-28 16:50:15 +0300

    Fix change other dice material

--- Assets/Scripts/Duel/Models/Dice.cs
+++ Assets/Scripts/Duel/Models/Dice.cs
@@ -7,21 +7,16 @@ namespace Duel.Models
 {
     public class Dice : MonoBehaviour
     {
-        [SerializeField] 
-        private Material _otherDiceMaterial;
-        
+        [SerializeField] private Material _otherDiceMaterial;
+
         private void Start()
         {
             var photonView = GetComponent<PhotonView>();
 
             if (!photonView.IsMine)
             {
-                var meshRenderer = GetComponentInChildren<MeshRenderer>();
-                for (var i = 0; i < meshRenderer.materials.Length; i++)
-                {
-                    Debug.Log(meshRenderer.materials[i]);
-                    meshRenderer.materials[i] = _otherDiceMaterial;
-                }
+                var renderer = GetComponentInChildren<Renderer>();
+                renderer.material = _otherDiceMaterial;
             }
         }
     }
