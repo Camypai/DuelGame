3b06b50909b55c12e5f2539909bfafe3cd588792
Илья <igorshkov.93@yandex.ru>
2020-05-29 17:14:24 +0300

    Fix bullet fly direction

--- Assets/Scripts/Duel/Entities/Bullet.cs
+++ Assets/Scripts/Duel/Entities/Bullet.cs
@@ -12,13 +12,14 @@ namespace Duel.Entities
         private readonly float _speed;
         private readonly Rigidbody _rigidbody;
         private readonly TrailRenderer _trailRenderer;
+        private float _direction;
 
         public Bullet(GameObject bulletObject, Vector3 startPosition, float speed)
         {
             _startPosition = startPosition;
             _speed = speed;
-            var rotate = PhotonNetwork.IsMasterClient ? Vector3.forward : Vector3.back;
-            _bullet = Object.Instantiate(bulletObject, _startPosition, Quaternion.LookRotation(rotate));
+            _direction = PhotonNetwork.IsMasterClient ? 0f : -180f;
+            _bullet = Object.Instantiate(bulletObject, _startPosition, Quaternion.Euler(new Vector3(0f, _direction, 0f)));
 
             _rigidbody = _bullet.GetComponent<Rigidbody>();
             _trailRenderer = _bullet.GetComponent<TrailRenderer>();
@@ -29,7 +30,8 @@ namespace Duel.Entities
         {
             Debug.Log("Fire");
             MoveToStartPosition();
-            _rigidbody.AddForce(Vector3.forward * _speed);
+            var direction = PhotonNetwork.IsMasterClient ? Vector3.forward : Vector3.back;
+            _rigidbody.AddForce(direction * _speed);
         }
 
         private void MoveToStartPosition()
@@ -42,7 +44,7 @@ namespace Duel.Entities
         public void Lose()
         {
             MoveToStartPosition();
-            _rigidbody.AddForce(new Vector3(0.6f,0f,1f) * _speed);
+            _rigidbody.AddForce(new Vector3(0.6f, 0f,0f) * _speed);
         }
     }
 }
